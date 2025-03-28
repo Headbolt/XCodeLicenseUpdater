6a04c0708d39de4f54c11e33efe12795996a5da9
Headbolt <54707369+Headbolt@users.noreply.github.com>
2025-03-28 12:07:12 +0000

    Update XCodeLicenseUpdater.sh

--- XCodeLicenseUpdater.sh
+++ XCodeLicenseUpdater.sh
@@ -18,12 +18,15 @@
 #
 # HISTORY
 #
-#	Version: 1.1 - 28/03/2025
+#	Version: 1.2 - 28/03/2025
 #
 #	28/03/2025 - V1.0 - Created by Headbolt
 #
 #	28/03/2025 - V1.1 - Updated by Headbolt
 #							Updated to include better reporting
+#
+#	28/03/2025 - V1.2 - Updated by Headbolt
+#							Updated to include extra checking
 #
 ###############################################################################################################################################
 #
@@ -31,7 +34,7 @@
 #
 ###############################################################################################################################################
 #
-ScriptVer=v1.1
+ScriptVer=v1.2
 ScriptName="Application | XCode License Updater"
 ExitCode=0
 #
@@ -52,6 +55,32 @@ SectionEnd
 /bin/echo # Outputting a Blank Line for Reporting Purposes
 /bin/echo  ----------------------------------------------- # Outputting a Dotted Line for Reporting Purposes
 /bin/echo # Outputting a Blank Line for Reporting Purposes
+#
+}
+#
+###############################################################################################################################################
+#
+# Gather Information Function
+#
+InfoGather(){
+#
+XcodeVers=$(defaults read "/Applications/xcode.app"/Contents/version CFBundleShortVersionString 2>/dev/null) # Extracts the Version from the APP
+if [[ -n "$XcodeVers" ]] # Check result, if blank set it to 0
+	then
+		/bin/echo "App Ver = $XcodeVers"
+	else
+		/bin/echo 'App Missing'
+		XcodeVers="0"
+fi
+#
+License=$(sudo defaults read "/Library/Preferences/com.apple.dt.Xcode.plist" IDEXcodeVersionForAgreedToGMLicense 2>/dev/null) # Extracts License Version
+if [[ -n "$License" ]] # Check result, if blank set it to 0
+	then
+		/bin/echo "License Ver = $License"
+	else
+		/bin/echo 'License Missing'
+		License="0"
+fi
 #
 }
 #
@@ -94,30 +123,16 @@ exit $ExitCode
 #
 ScriptStart
 #
-XcodeVers=$(defaults read "/Applications/xcode.app"/Contents/version CFBundleShortVersionString 2>/dev/null) # Extracts the Version from the APP
-if [[ -n "$XcodeVers" ]] # Check result, if blank set it to 0
-	then
-		/bin/echo "App Ver = $XcodeVers"
-	else
-		/bin/echo 'App Missing'
-		XcodeVers="0"
-fi
-#
-License=$(sudo defaults read "/Library/Preferences/com.apple.dt.Xcode.plist" IDEXcodeVersionForAgreedToGMLicense 2>/dev/null) # Extracts License Version
-if [[ -n "$License" ]] # Check result, if blank set it to 0
-	then
-		/bin/echo "License Ver = $License"
-	else
-		/bin/echo 'License Missing'
-		License="0"
-fi
-#
-SectionEnd
+/bin/echo 'Checking current details'
+/bin/echo # Outputting a Blank Line for Reporting Purposes
+InfoGather
 #
 if [[ "$XcodeVers" == "0" ]] # Check App Present
 	then
+		SectionEnd
     	/bin/echo 'App Missing - Nothing To Do'
 	else
+		SectionEnd
 		if [[ "$License" == "0" ]] # Check License Present
 			then
 				/bin/echo 'App Present but License Missing - Applying License'
@@ -128,11 +143,24 @@ if [[ "$XcodeVers" == "0" ]] # Check App Present
 				/bin/echo 'Running Command "'/Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -runFirstLaunch'"'
 				/bin/echo # Outputting a Blank Line for Reporting Purposes
                 /Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -runFirstLaunch
+				SectionEnd
+				/bin/echo 'Re-Checking current details'
+				/bin/echo # Outputting a Blank Line for Reporting Purposes
+				InfoGather
 			else
 				if [[ "$License" < "$XcodeVers" ]] # Check if License Version is older than the App Version
 					then
-						/bin/echo 'License Update Needed - Applying License'
+						/bin/echo 'License Update Needed - Removing Current License'
+						/bin/echo 'Running Command "'rm /Library/Preferences/com.apple.dt.Xcode.plist'"'
+						rm /Library/Preferences/com.apple.dt.Xcode.plist
+						/bin/echo # Outputting a Blank Line for Reporting Purposes
+                        /bin/echo 'License Update Needed - Applying License'
+						/bin/echo 'Running Command "'/Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -license accept'"'
 						/Applications/Xcode.app/Contents/Developer/usr/bin/xcodebuild -license accept
+						SectionEnd
+						/bin/echo 'Re-Checking current details'
+						/bin/echo # Outputting a Blank Line for Reporting Purposes
+						InfoGather
 				fi
 		fi
 fi
